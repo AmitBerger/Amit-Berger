2a8d4ecd7a24389e39f6b3a9d4b18fc7f6db57aa
AmitBerger <92735064+AmitBerger@users.noreply.github.com>
2021-11-08 16:15:36 +0200

    Update advancedClassificationRecursion.c

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 
-int  isPalindrome(int n){
-    static int sum = 0;
-    if(n != 0)
-    {
-        sum = sum *10 + n%10;
-         isPalindrome(n/10);   
-    }
-    else if(sum == n)
-        return 1;
-    else
+
+int reverse(int num)
+{
+    /* Find number of digits in num */
+    int digit = (int)log10(num);
+    
+    /* Recursion base condition */
+    if(num == 0)
         return 0;
+
+    return ((num%10 * pow(10, digit)) + reverse(num/10));
 }
+int isPalindrome(int num){
+    
+    if(num == reverse(num))
+    {
+        return 1;
+    }
+    
+    return 0;
+}
+
+
 
 
 int power(int base, int exp){
