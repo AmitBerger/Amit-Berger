abb353c3c139184e5889a2faec4ef1c8ab196d8b
AmitBerger <92735064+AmitBerger@users.noreply.github.com>
2021-11-08 16:43:24 +0200

    Update basicClassification.c

--- basicClassification.c
+++ basicClassification.c
@@ -1,5 +1,31 @@
-#include <stdio.h>  
-int fact(int  n){
+# include <stdio.h>
+
+int isPrime(int n){
+
+    int flag=1;
+        if (n<1){
+            flag =0;
+        }
+        else if( n==1){
+            flag=1;
+        }
+        else{
+            for (int i=2; i<=n/2 && flag!=0; i++ ){
+                if (n%i==0){
+                    flag=0 ;
+                }
+            }
+        }
+        if (flag==1){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+    
+
+int factorial(int  n){
     if (n==0){
         return 1;
     }
@@ -10,45 +36,26 @@ int fact(int  n){
         return n*factorial( n-1);
     }
 }
-int isStrong(int n)     {  
-  
-    int sum=0;  
-    int k=n;  
-    int r;  
-    while(k!=0)  {  
-        r=k%10;  
-        int f=fact(r);  
-        k=k/10;  
-        sum=sum+f;  
-    }  
-    if(sum==n)   {  
-        return 1; 
-    }  
-    else  {  
-        return 0; 
-    }  
-    return 0;  
-}  
 
 
-int isPrime(int n){
-	int flag =1;
-	for (int i = 1; i <= n-1; i++) {
-        if (n % i == 0) {
-            flag = 0;
-            break;
-        }
+int isStrong(int n){
+
+    int x=0;
+    int ans=0;
+    int m=n;
+    if (n==0){
+        ans=1;
     }
- 
-    if (n <= 1)
-        flag = 0;
- 
-    if (flag == 1) {
+    while (m>=1){
+        x=m%10;
+        ans=ans+factorial(x);
+        m=m/10;
+    }
+    if (n==ans){
         return 1;
     }
-    else {
+    else{
         return 0;
     }
- 
-    return 0;
-	}
+}
+
