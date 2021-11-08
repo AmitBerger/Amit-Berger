e4b7dca6b2c4b1cd955cdb0b6af1ba96e3333fef
AmitBerger <92735064+AmitBerger@users.noreply.github.com>
2021-11-08 16:42:56 +0200

    Update advancedClassificationRecursion.c

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,64 +1,83 @@
-#include <stdio.h>
+# include <stdio.h>
+/*# include <math.h>*/
 
-
-int reverse(int num)
-{
-    /* Find number of digits in num */
-    int digit = (int)log10(num);
-    
-    /* Recursion base condition */
-    if(num == 0)
-        return 0;
-
-    return ((num%10 * pow(10, digit)) + reverse(num/10));
+int power(int base, int n){
+    int ans=base;
+    for (int i=0;i<n-1;i++){
+        ans=ans*base;
+    }
+    return ans;
 }
-int isPalindrome(int num){
-    
-    if(num == reverse(num))
-    {
+
+
+
+int isArmstrongRecursive(int n, int ans,int m, int countnow, int count){
+    int x=0;
+    if (countnow==count){
+        if (ans==n){
+            return 1;
+         }
+        else{
+            return 0;
+        }
+    } 
+    else{
+        countnow=countnow+1;
+        x=m%10;
+        m=m/10;
+        ans=ans+power(x, count);
+        return isArmstrongRecursive(n,ans,m,countnow,count);
+    }
+}
+
+
+
+int isArmstrong(int n){
+    int count=0;
+    int m=n;
+    int ans=0;
+    while (m>=1){
+        m=m/10;
+        count=count+1;
+    }
+    m=n;
+    return isArmstrongRecursive(n, ans,m, 0, count);
+
+
+}
+
+int IsPolindromeRecursive(int start,int end,int arr[]){
+    if (start>= end){
         return 1;
     }
-    
-    return 0;
+    else{
+        if (arr[start]==arr[end]){
+            start=start+1;
+            end=end-1;
+            return IsPolindromeRecursive(start,end,arr);
+        }
+        else{
+            return 0;
+        }
+    }
 }
 
 
 
 
-int power(int base, int exp){
-	int result=base;
-for(int i=0; i<exp-1;i++){
-	result=result*base;
-	}
-return result;
-}
-int  isArmstrongRec(int n, int ans, int temp,int times,int Digits){
-	int k =0;
-	if(times==Digits){
-		if(ans==n){
-			return 1;
-		}
-		else {return 0;}
-	}
-		else{
-			times=times+1;
-			k=temp%10;
-			temp=temp/10;
-			ans=ans+power(k,Digits);
-			return isArmstrongRec(n, ans,temp, times,Digits);
-	}
-}
+int isPalindrome(int n){
+    int m=n;
+    int count=0;
+    while (m>=1){
+        m=m/10;
+        count=count+1;
+    }
+    int arr[count];
+    m=n;
+    for (int i=0; i<count; i++){
+        arr[i]=m%10;
+        m=m/10;
+    }
+return IsPolindromeRecursive(0,count-1,arr);
 
-
-int isArmstrong(int n) {
-	int Digits =0;
-	int temp =n;
-	int answer =0;
-	while (temp>=1){
-		temp=temp/10;
-		Digits=Digits+1;
-	}
-	temp =n;
-	return isArmstrongRec(n, answer, temp,0,Digits);
-	
 }
