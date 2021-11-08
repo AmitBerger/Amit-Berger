b8d328ed11260ddf01b5c5a568b705d3542bf757
AmitBerger <92735064+AmitBerger@users.noreply.github.com>
2021-11-08 19:22:30 +0200

    Update basicClassification.c

--- basicClassification.c
+++ basicClassification.c
@@ -1,61 +1,53 @@
 # include <stdio.h>
-
 int isPrime(int n){
-
-    int flag=1;
-        if (n<1){
-            flag =0;
-        }
-        else if( n==1){
-            flag=1;
-        }
-        else{
-            for (int i=2; i<=n/2 && flag!=0; i++ ){
-                if (n%i==0){
-                    flag=0 ;
-                }
-            }
-        }
-        if (flag==1){
-            return 1;
-        }
-        else{
-            return 0;
-        }
-    }
-    
-
-int factorial(int  n){
-    if (n==0){
-        return 1;
-    }
-    else if (n==1){
-        return 1;
-    }
-    else{
-        return n*factorial( n-1);
-    }
+	int i;
+	for(i=2;i<=n/2;i++)
+	{
+		if(n%i!=0)
+			continue;
+		else
+			return 1;
+	}
+	return 0;
 }
 
+int isStrong(int num)
+{
+    int originalNum, lastDigit, sum;
 
-int isStrong(int n){
+    originalNum = num;
 
-    int x=0;
-    int ans=0;
-    int m=n;
-    if (n==0){
-        ans=1;
+    sum = 0;
+
+    while(num > 0)
+    {
+
+       
+        lastDigit = num % 10;
+
+        /* Find factorial of last digit */
+        int fact = 1;
+        for(int i=1; i<=lastDigit; i++)
+        {
+            fact = fact * i;
+        }
+
+        /* Add factorial to sum */
+        sum = sum + fact;
+
+        num = num / 10;
     }
-    while (m>=1){
-        x=m%10;
-        ans=ans+factorial(x);
-        m=m/10;
+
+    /* Check Strong number condition */
+    if(sum == originalNum)
+    {
+       return 1;
     }
-    if (n==ans){
-        return 1;
-    }
-    else{
+    else
+    {
         return 0;
     }
+
+    return 0;
 }
 
